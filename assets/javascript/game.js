44801402e9911cc4a98a9a584573a76d24520e7c
jamesk111 <8887227+jamesk111@users.noreply.github.com>
2018-08-21 22:25:12 -0500

    added difficulty selection

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,20 +1,30 @@
 //Initialize
-var difficultySelection = 1;
-
 var wordArray = [
     ["Cow", "Cat", "Moth", "Crow", "Trap", "Wack", "Lake", "Rip", "Lack", "Reap"], //Easy Words
     ["Turtle", "Heroku", "Missile", "Racket", "Rocket", "Trippy", "Roland", "Marker", "Spacey", "Lemonade"], //Intermediate Words
     ["Jazz", "Origami", "Awkward", "Crypt", "Dwarves", "Fjord", "Kiosk", "Ostracize", "Queue", "Zombie"] //Hard Words
 ];
 
-//Build our easy list
+var gameWords;
+
 function buildWords(difficulty) {
-    var words = [];
-    for (i = 0; i < wordArray[difficulty - 1].length; i++) {
+    let words = [];
+    for (i = 0; i < wordArray[difficulty].length; i++) {
         let wordEntry = {};
-        wordEntry.text = wordArray[difficulty - 1][i];
-        wordEntry.guessLimit = wordArray[difficulty - 1][i].length;
+        wordEntry.text = wordArray[difficulty][i];
+        wordEntry.guessLimit = wordArray[difficulty][i].length;
         words.push(wordEntry);
     }
     return words;
 }
+
+function setDifficulty() {
+    let difficulty = document.getElementsByName("difficulty");
+    for (i = 0; i < difficulty.length; i++) {
+        if (difficulty[i].checked) {
+            gameWords = buildWords(i);
+        }
+    }
+}
+
+setDifficulty();
