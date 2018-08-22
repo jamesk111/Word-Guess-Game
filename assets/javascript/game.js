f4f5b28a8a8b978b9635a480b2bee48d510f9746
jamesk111 <8887227+jamesk111@users.noreply.github.com>
2018-08-22 00:00:09 -0500

    updated javascript for difficulty

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,26 +5,36 @@ var wordArray = [
     ["Jazz", "Origami", "Awkward", "Crypt", "Dwarves", "Fjord", "Kiosk", "Ostracize", "Queue", "Zombie"] //Hard Words
 ];
 
-var gameWords;
+var game = {
 
-function buildWords(difficulty) {
-    let words = [];
-    for (i = 0; i < wordArray[difficulty].length; i++) {
-        let wordEntry = {};
-        wordEntry.text = wordArray[difficulty][i];
-        wordEntry.guessLimit = wordArray[difficulty][i].length;
-        words.push(wordEntry);
-    }
-    return words;
-}
+    gameWords: [],
 
-function setDifficulty() {
-    let difficulty = document.getElementsByName("difficulty");
-    for (i = 0; i < difficulty.length; i++) {
-        if (difficulty[i].checked) {
-            gameWords = buildWords(i);
+    gameLevels: [],
+
+    buildWords: function buildWords(difficulty, array) {
+        let words = [];
+        for (i = 0; i < array[difficulty].length; i++) {
+            let wordEntry = {};
+            wordEntry.text = array[difficulty][i];
+            wordEntry.guessLimit = array[difficulty][i].length;
+            words.push(wordEntry);
         }
-    }
-}
+        this.gameWords = words;
+    },
 
-setDifficulty();
+    setDifficulty: function setDifficulty(difficulty) {
+        this.buildWords(difficulty, this.gameLevels);
+    }
+};
+
+window.onload = function () {
+    game.gameLevels = wordArray;
+    // document.getElementById("easy").addEventListener("change", game.setDifficulty(0));
+    // document.getElementById("intermediate").addEventListener("change", game.setDifficulty(1));
+    // document.getElementById("hard").addEventListener("change", game.setDifficulty(2));
+    // var difficulty = document.getElementById("difficulty-wrapper");
+    // for (i = 0; i < difficulty.length; i++) {
+    //     difficulty.onclick
+    // }
+    game.setDifficulty(0);
+};
