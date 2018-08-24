3fe0635e986023fcd89e38b157beab3623afe5c5
jamesk111 <8887227+jamesk111@users.noreply.github.com>
2018-08-24 18:54:38 -0500

    updated button functionality and added prototype function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,6 +12,7 @@ function Game() {
     this.guessLimit = 0;
     this.guessList = "";
     this.answerDisplay = "";
+    this.outcome = "";
     this.wins = 0;
     this.losses = 0;
 
@@ -36,52 +37,71 @@ function Game() {
 
     this.setWord = function () {
         let getWord = this.level[Math.floor(Math.random() * this.level.length)];
+        this.guessLimit = getWord.guessLimit;
+        this.guessCount = 0;
+        this.guessList = "";
+        this.outcome = "";
         this.word = getWord.text.toLowerCase();
         this.answerDisplay = Array(this.word.length + 1).join("_");
     };
 
     this.makeGuess = function (letter) {
-        let answer = this.word.split("");
+        // let answer = this.word.split("");
         let display = this.answerDisplay.split("");
         letter = letter.toLowerCase();
 
-        //Determine if the letter is part of the word or not.
-        if (this.word.indexOf(letter) >= 0) {
-            for (i = 0; i < answer.length; i++) {
-                if (letter === answer[i]) {
-                    display[i] = letter.toUpperCase();
+        //Get out if we're already finished with the current game.
+        if (!this.outcome) {
+
+            //Determine if the letter is part of the word or not.
+            if (this.word.indexOf(letter) >= 0) {
+                for (i = 0; i < this.word.length; i++) {
+                    if (letter === this.word[i]) {
+                        display[i] = letter.toUpperCase();
+                    }
                 }
+            } else if (this.guessList.indexOf(letter.toUpperCase()) < 0) {
+                this.guessCount++;
+                this.guessList += letter.toUpperCase();
             }
-        } else if (this.guessList.indexOf(letter.toUpperCase()) < 0) {
-            this.guessCount++;
-            this.guessList += letter.toUpperCase();
-        }
 
-        //Update the answer display
-        this.answerDisplay = display.join("");
+            //Update the answer display
+            this.answerDisplay = display.join("");
 
-        //Did the player win/lose?
-        if (this.answerDisplay.toLowerCase() === this.word) {
-            this.wins++;
-        } else if (this.guessCount === this.guessLimit) {
-            this.losses++;
+            //Did the player win/lose?
+            if (this.answerDisplay.toLowerCase() === this.word) {
+                this.wins++;
+                this.outcome = "win";
+            } else if (this.guessCount === this.guessLimit) {
+                this.losses++;
+                this.outcome = "lose";
+            }
         }
     }
+}
+
+//Lets add a new function using the prototype
+Game.prototype.giveUp = function () {
+    this.losses++;
+    this.outcome = "lose";
+    this.answerDisplay = this.word.toUpperCase();
 }
 
 //This function builds the game display form the game object.
 function buildGameDisplay() {
     document.getElementById("answer-display").textContent = game.answerDisplay;
+    document.getElementById("answer-display").style.color = "black";
     document.getElementById("guess-display").textContent = "None!";
 }
 
+//This function updates the scores on the screen.
 function updateScore() {
     document.getElementById("wins").textContent = game.wins;
     document.getElementById("losses").textContent = game.losses;
 }
 
 //Event listener for keystrokes.
-window.onkeyup = function (e) {
+document.addEventListener("keyup", function (e) {
 
     //We only want to process for alpha characters
     if (e.key.match(/^[a-zA-Z]\b/)) {
@@ -90,22 +110,48 @@ window.onkeyup = function (e) {
         if (game.guessList) {
             document.getElementById("guess-display").textContent = game.guessList;
         }
-        updateScore();
+        if (game.outcome) {
+            updateScore();
+            if (game.outcome === "win") {
+                document.getElementById("answer-display").style.color = "limegreen";
+            } else {
+                document.getElementById("answer-display").textContent = game.word.toUpperCase();
+                document.getElementById("answer-display").style.color = "red";
+            }
+        }
     }
-};
 
-// });
+});
 
 //Event listener for radio buttons
 var difficulties = document.querySelectorAll(".diffButton");
 difficulties.forEach(function (difficulty, difficultyIndex) {
-    // difficulty.addEventListener("click", function () {
-    difficulty.click = function () {
+    difficulty.addEventListener("click", function () {
         if (difficultyIndex !== game.difficulty) {
             game.setDifficulty(difficultyIndex);
             buildGameDisplay();
         }
-    };
+    });
+});
+
+//Game Buttons (Next Word / Give Up)
+var gameButton = document.querySelectorAll(".action-btn");
+gameButton.forEach(function (btn, index) {
+    btn.addEventListener("click", function() {
+        if (btn.id === "give-up") {
+            game.giveUp();
+            updateScore();
+            document.getElementById("answer-display").textContent = game.word.toUpperCase();
+            document.getElementById("answer-display").style.color = "red";
+        } else if (btn.id === "next-word") {
+            if (!game.outcome) {
+                game.giveUp();
+                updateScore();
+            }
+            game.setWord();
+            buildGameDisplay();
+        }
+    });
 });
 
 //Build our game
