1c4d91d475a71d35e3ee78fd0408ae73e23e4649
jamesk111 <8887227+jamesk111@users.noreply.github.com>
2018-08-27 19:28:18 -0500

    update polyfill to match MDN

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,11 @@
 //Polyfill for forEach on NodeList
-if (!NodeList.prototype.forEach) {
-    NodeList.prototype.forEach = Array.prototype.forEach;
+if (window.NodeList && !NodeList.prototype.forEach) {
+    NodeList.prototype.forEach = function (callback, thisArg) {
+        thisArg = thisArg || window;
+        for (var i = 0; i < this.length; i++) {
+            callback.call(thisArg, this[i], i, this);
+        }
+    };
 }
 
 //Game Constructor
